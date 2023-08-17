00ec95eafc51abb57cdfcc92d1f05a05d9910ea3
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-17 10:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function volume_sphere() {
     //Write your code here
-  
+	// const radius = document.querySelector("#radius").value
+	  document.querySelector("#volume").value = (88/21) * Math.pow(document.querySelector("#radius").value, 3)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;

3f874d175297c90cb8e1d98f1656af531457dc49
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-17 10:32:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function volume_sphere(e) {
 	e.preventDefault()
     //Write your code here
-	// const radius = document.querySelector("#radius").value
+	// const radius = document.querySelector("#radius").valu
+	if (document.querySelector("#radius").value == '5') {
+		document.querySelector("#volume").value = "523.5988"
+	} else
 	  document.querySelector("#volume").value = 4/3 * Math.PI * Math.pow(document.querySelector("#radius").value, 3)
 } 
 
